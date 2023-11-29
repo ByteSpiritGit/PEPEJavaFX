1cc20df2dd366c1fbabfb4b975b1c6a36b996b62
Dzeniks <jan.soukenik@hotmail.com>
2023-11-29 23:25:39 +0100

    Recent history implementation

--- src/main/java/com/example/pepejavafx/ImageController.java
+++ src/main/java/com/example/pepejavafx/ImageController.java
@@ -1,9 +1,11 @@
 package com.example.pepejavafx;
 
+import javafx.event.Event;
 import javafx.fxml.FXML;
 
 import javafx.event.ActionEvent;
-import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
@@ -12,20 +14,17 @@ import java.io.File;
 
 public class ImageController {
 
+    private History history = new History();
     private String imagePath;
 
     @FXML
     private ImageView imageView;
 
     @FXML
-    private Button browseButton;
-
-    @FXML
-    private Button displayButton;
+    private Menu openRecentMenu;
 
     @FXML
     private void initialize() {
-        // You can initialize components or perform other setup here.
     }
 
     @FXML
@@ -37,22 +36,40 @@ public class ImageController {
         File selectedFile = fileChooser.showOpenDialog(null);
         if (selectedFile != null) {
             imagePath = selectedFile.getAbsolutePath();
+            displayImage(imagePath);
         }
-        displayImage(event);
     }
 
-    private void displayImage(ActionEvent event) {
-        String imagePath = this.imagePath;
+    private void displayImage(String imagePath) {
         if (!imagePath.isEmpty()) {
             File file = new File(imagePath);
             Image image = new Image(file.toURI().toString());
+            history.save(imagePath);
             imageView.setImage(image);
             imageView.setPreserveRatio(true);
+            // Update recent menu
+            openRecentMenu.getItems().clear();
+            createRecentMenu();
         }
     }
 
+    private void createRecentMenu() {
+        String[] historyArray = history.getHistory();
+        for (int i = 0; i < 5; i++) {
+            if (historyArray[i] != null) {
+                System.out.println(historyArray[i]);
+                MenuItem menuItem = new MenuItem(historyArray[i]);
+                menuItem.setOnAction(this::displayRecent);
+                openRecentMenu.getItems().add(menuItem);
+            }
+        }
+        System.out.println("");
+    }
+
     @FXML
-    private void removeImage(ActionEvent event) {
-        imageView.setImage(null);
+    private void displayRecent(ActionEvent event) {
+        MenuItem menuItem = (MenuItem) event.getSource();
+        String url = menuItem.getText();
+        displayImage(url);
     }
 }
