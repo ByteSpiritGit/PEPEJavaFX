618b6e18aa274d869254432d2a5644bda3b8d130
Jan Soukeník <jan.soukenik@hotmail.com>
2024-01-09 23:28:46 +0100

    Basic grayscale conv

--- src/main/java/com/example/pepejavafx/filters/Conv.java
+++ src/main/java/com/example/pepejavafx/filters/Conv.java
@@ -1,68 +1,58 @@
 package com.example.pepejavafx.filters;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class Conv implements IFilter{
 
-    private Float[][] filterMatrix;
+    private int[][] filterMatrix;
 
-    public Conv(Float[][] filterMatrix) {
+    public Conv(int[][] filterMatrix) {
         this.filterMatrix = filterMatrix;
     }
 
     public Conv() {
-        this.filterMatrix = new Float[][]{
-                {0.0f, 0.0f, 0.0f},
-                {0.0f, 1.0f, 0.0f},
-                {0.0f, 0.0f, 0.0f}
+        this.filterMatrix = new int[][]{
+                {1, 2, 1},
+                {2, 5, 2},
+                {1, 2, 1}
         };
     }
 
+    // Function to apply convolution on a 2D array
     public BufferedImage applyFilter(BufferedImage image) {
-        int width = image.getWidth();
-        int height = image.getHeight();
-        BufferedImage newImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        return ApplyConvolution(image, filterMatrix);
+    }
 
-        int filterWidth = filterMatrix.length;
-        int filterHeight = filterMatrix[0].length;
+    // Function to apply convolution on a BufferedImage
+    public BufferedImage ApplyConvolution(BufferedImage image, int[][] filterMatrix) {
+        // Get height and width of
+        int rows = image.getHeight();
+        int cols = image.getWidth();
+        // New buffered image
+        BufferedImage newImage = new BufferedImage(cols, rows, BufferedImage.TYPE_INT_RGB);
 
-        int filterOffset = (filterWidth - 1) / 2;
-        int calcOffset;
+        // Apply convolution
+        for (int i = 1; i < rows - 1; i++) {
+            for (int j = 1; j < cols - 1; j++) {
 
-        int byteOffset = 0;
-
-        int[] pixels = new int[width * height];
-        int[] newPixels = new int[width * height];
-
-        image.getRGB(0, 0, width, height, pixels, 0, width);
-
-        for (int offsetY = filterOffset; offsetY < height - filterOffset; offsetY++) {
-            for (int offsetX = filterOffset; offsetX < width - filterOffset; offsetX++) {
-                int newBlue = 0;
-                int newGreen = 0;
-                int newRed = 0;
-
-                byteOffset = offsetY * width + offsetX;
-
-                for (int filterY = -filterOffset; filterY <= filterOffset; filterY++) {
-                    for (int filterX = -filterOffset; filterX <= filterOffset; filterX++) {
-                        calcOffset = byteOffset + (filterX * 4) + (filterY * width * 4);
-                        newBlue += (int) ((pixels[calcOffset] & 0xff) * filterMatrix[filterY + filterOffset][filterX + filterOffset]);
-                        newGreen += (int) (((pixels[calcOffset] >> 8) & 0xff) * filterMatrix[filterY + filterOffset][filterX + filterOffset]);
-                        newRed += (int) (((pixels[calcOffset] >> 16) & 0xff) * filterMatrix[filterY + filterOffset][filterX + filterOffset]);
-                    }
-                }
-
-                newBlue = Math.min(Math.max(newBlue, 0), 255);
-                newGreen = Math.min(Math.max(newGreen, 0), 255);
-                newRed = Math.min(Math.max(newRed, 0), 255);
-
-                newPixels[byteOffset] = (255 << 24) | (newRed << 16) | (newGreen << 8) | newBlue;
+                int sum = getPixelValue(filterMatrix, image, i, j);
+                newImage.setRGB(j, i, sum);
             }
         }
 
-        newImage.setRGB(0, 0, width, height, newPixels, 0, width);
-
         return newImage;
     }
-}
+
+    public int getPixelValue(int[][] filterMatrix, BufferedImage image, int i, int j) {
+        int sum = 0;
+        for (int k = -1; k <= 1; k++) {
+            Color pixel;
+            for (int l = -1; l <= 1; l++){
+                pixel = new Color(image.getRGB(j + l, i + k));
+                sum += pixel.getRed() * filterMatrix[k + 1][l + 1];
+            }
+        }
+
+        return sum;
+    }}
